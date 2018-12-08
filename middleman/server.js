678bdf27320d6ee92d3b95e62ec2f59164cd5cf5
Deon <deonsummers91@gmail.com>
2018-12-08 16:27:04 -0600

    add other endpoint

--- middleman/server.js
+++ middleman/server.js
@@ -1,12 +1,18 @@
 const app = require('express')();
 const request = require('request');
+const { request: gqlrequest } = require('graphql-request')
 
-
-app.get('/', (req, resp) => {
-  request('http://api.oasisdex.com/v1/pairs/', (err, res, body) => {
+app.get('/prices', (req, resp) => {
+  request('http://api.oasisdex.com/v1/prices/eth/dai', (err, res, body) => {
     resp.send(body);
   });
 });
 
+app.get('/cups/:id', async (req, resp) => {
+  const query = `{getCup(id: ${req.params.id}) {ratio}}`
+
+  gqlrequest('https://sai-mainnet.makerfoundation.com/v1', query)
+    .then(data => resp.send(data))
+});
 
 app.listen(8080);
